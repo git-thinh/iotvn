09062e075f52f1400845efd063712f17383201ce
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-11-23 16:29:33 +0700

    test

---
+++ iotvn/Core/TcpServer/_Test/test2.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (C) 2016 by Barend Erasmus and donated to the public domain
+
+using SimpleHttpServer;
+using SimpleHttpServer.Models;
+using SimpleHttpServer.RouteHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+//using System.Threading.Tasks;
+
+namespace iotvn
+{
+    static class Routes
+    {
+
+        public static List<Route> GET
+        {
+            get
+            {
+                return new List<Route>()
+                {
+                    new Route()
+                    {
+                        Callable = HomeIndex,
+                        UrlRegex = "^\\/$",
+                        Method = "GET"
+                    },
+                    new Route()
+                    {
+                        Callable = new FileSystemRouteHandler() { BasePath = @"C:\Users\Barend.Erasmus\Desktop\Test"}.Handle,
+                        UrlRegex = "^\\/Static\\/(.*)$",
+                        Method = "GET"
+                    }
+                };
+
+            }
+        }
+
+        private static HttpResponse HomeIndex(HttpRequest request)
+        {
+            return new HttpResponse()
+            {
+                ContentAsUTF8 = "Hello",
+                ReasonPhrase = "OK",
+                StatusCode = "200"
+            };
+
+        }
+    }
+
+    class test2
+    {
+        static void run(string[] args)
+        {
+            HttpServer httpServer = new HttpServer(8080, Routes.GET);
+
+            Thread thread = new Thread(new ThreadStart(httpServer.Listen));
+            thread.Start();
+        }
+    }
+}
