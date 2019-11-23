a4ecdbd6e586f3a766b00b54a4948ac844b9f11c
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-11-23 08:14:07 +0700

    init project iot.vn

---
+++ iotvn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iotvn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
